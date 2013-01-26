5ed892becb62708ef64ffd82d618c2a6f9f75776
Chase Blokker <chaseblokker@yahoo.com>
2013-01-26 14:25:18 -0800

    A brown hexagon. Can change numberOfSides variable to draw different sided polygons.

---
+++ homework/canvas-basics/hexagon/25e.js
@@ -0,0 +1,27 @@
+/*
+ * Chase Blokker
+ * CMSI 371
+ * Assignment 0129
+ * Problem 25(e): A solid, brown hexagon
+ * 01/29/13
+ */
+ 
+(function () {
+    var canvas = document.getElementById('canvas');
+        var renderingContext = canvas.getContext("2d");
+        
+        // Variables for defining polygon shape
+        var numberOfSides = 6,
+            size = 100,
+            Xcenter = 200,
+            Ycenter = 200;
+        
+        // Itterative formula for polygon
+        for (var i = 1; i <= numberOfSides; i++) {
+            renderingContext.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+        }
+
+        renderingContext.fillStyle = "#802A2A";
+        renderingContext.fill();
+
+}());
