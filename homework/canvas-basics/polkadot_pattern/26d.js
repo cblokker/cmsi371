821ce4364c09683b92617bf0f50a3cc16965673e
Chase Blokker <chaseblokker@yahoo.com>
2013-01-28 23:23:37 -0800

    Cleaned up code.  Created arrays for the colors and color stops.

--- homework/canvas-basics/polkadot_pattern/26d.js
+++ homework/canvas-basics/polkadot_pattern/26d.js
@@ -8,48 +8,41 @@
  */
 
 (function () {
-    var canvas = document.getElementById('canvas');
-        var renderingContext = canvas.getContext("2d");
+    var canvas = document.getElementById('canvas'),
+        renderingContext = canvas.getContext("2d"),
 
-        // Background color
-        renderingContext.fillStyle = 'brown';
-        renderingContext.fillRect (0, 0, 512, 512);
+        // Variables for the circular dot pattern
+        numOfRings = 39,
+        colorStop = [0, 25, 50, 75, 100, 125, 150, 175],
+        colorName = ['red','orange','yellow','green','blue','indigo','violet'];
 
-        renderingContext.translate(256,256);
+    // Background color
+    renderingContext.fillStyle = 'brown';
+    renderingContext.fillRect (0, 0, 512, 512);
+    
+    // Center the spiral
+    renderingContext.translate(256,256);
 
-        // Loop through rings
-        for (var i = 1; i < 39; i++) {
+    // Loop through rings
+    for (var i = 0; i < numOfRings; i++) {
 
-            // slowly increase the size of the circular dots for every ring itteration
-            renderingContext.scale(1.05,1.05);
+        // slowly increase the size of the circular dots for every ring itteration
+        renderingContext.scale(1.05,1.05);
 
-            // Draw individual dots
-            for (var j = 4; j < (i * 6); j++) {
+        // Draw individual dots
+        for (var j = 4; j < (i * 6); j++) {
 
-                // create rainbow appearance for dots
-                if (j <= 16) {
-                    renderingContext.fillStyle = 'red';
-                } else if ((j > 14) && (j <= 34)) {
-                    renderingContext.fillStyle = 'orange';
-                } else if ((j > 34) && (j <= 54)) {
-                    renderingContext.fillStyle = 'yellow';
-                } else if ((j > 54) && (j <= 74)) {
-                    renderingContext.fillStyle = 'green';
-                } else if ((j > 74) && (j <= 94)) {
-                    renderingContext.fillStyle = 'blue';
-                } else if ((j > 94) && (j <= 114)) {
-                    renderingContext.fillStyle = 'indigo';
-                } else if ((j > 114) && (j <= 150)) {
-                    renderingContext.fillStyle = 'violet';
-                } else {
-                    renderingContext.fillStyle = 'pink';
+            //create rainbow appearance for dots
+            for (var k = 0; k <= colorName.length; k++) {
+                if ((j > colorStop[k]) && (j <= colorStop[k + 1]) ) {
+                    renderingContext.fillStyle = colorName[k];
                 };
+            };
 
-                renderingContext.rotate(Math.PI * 2 / (i * 6));
-                renderingContext.beginPath();
-                renderingContext.arc(0, i, 0.5, 0, Math.PI * 2, true);
-                renderingContext.fill();
-                
-            }
-        }
+            renderingContext.rotate(Math.PI * 2 / (i * 6));
+            renderingContext.beginPath();
+            renderingContext.arc(0, i, 0.5, 0, Math.PI * 2, true);
+            renderingContext.fill();
+        };
+    };
 }());
