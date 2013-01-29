dfb857b41f498b51d43abf7ea57ecb646784d797
Chase Blokker <chaseblokker@yahoo.com>
2013-01-28 22:41:17 -0800

    cleaned up code and renamed variables.

--- homework/canvas-basics/honeycomb/26c.js
+++ homework/canvas-basics/honeycomb/26c.js
@@ -8,35 +8,35 @@
 
 (function () {
     var canvas = document.getElementById('canvas');
-
-        var renderingContext = canvas.getContext("2d");
+        renderingContext = canvas.getContext("2d");
  
         // Variables for defining polygon shape
-        var numberOfSides = 6,
-            size = 100,
-            Xcenter = 200,
-            Ycenter = 200,
-            Xdimention = 5,
-            Ydimention = 4;
+        numberOfSides = 6,
+        size = 100,
+        Xcenter = 200,
+        Ycenter = 200,
+        Xdimention = 5,
+        Ydimention = 4;
         
-        //draw collumns
-        for( var j = 0; j < Ydimention; j++) {
+    //draw collumns
+    for (var collumn = 0; collumn < Ydimention; collumn++) {
 
-        	//draw rows
-            for( var k = 0; k < Xdimention; k++) {
+        //draw rows
+        for (var row = 0; row < Xdimention; row++) {
 
-                //Draws the individual polygon
-                renderingContext.beginPath();
+            //Draws the individual polygon
+            renderingContext.beginPath();
 
-        	    for (var i = 0; i <= numberOfSides; i++) {
-                    renderingContext.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides) + 150 * k, Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides)  + 173 * j + 86 * k);
-                }
-
-                renderingContext.strokeStyle = "orange";
-                renderingContext.lineWidth = 10;
-                renderingContext.fillStyle = "yellow";
-                renderingContext.fill();
-                renderingContext.stroke();
+        	for (var i = 0; i <= numberOfSides; i++) {
+                renderingContext.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides) + 150 * row,
+                                         Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides) + 173 * collumn + 86 * row);
             }
+
+            renderingContext.strokeStyle = "orange";
+            renderingContext.lineWidth = 10;
+            renderingContext.fillStyle = "yellow";
+            renderingContext.fill();
+            renderingContext.stroke();
         }
+    }
 }());
