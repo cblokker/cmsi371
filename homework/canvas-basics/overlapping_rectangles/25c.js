6b2e406e9e7fb8252bef7dde60af07af0f80b899
Chase Blokker <chaseblokker@yahoo.com>
2013-01-28 22:34:01 -0800

    code cleanup.

--- homework/canvas-basics/overlapping_rectangles/25c.js
+++ homework/canvas-basics/overlapping_rectangles/25c.js
@@ -8,11 +8,11 @@
 
 (function () {
     var canvas = document.getElementById('canvas');
-        var renderingContext = canvas.getContext("2d");
+        renderingContext = canvas.getContext("2d");
  
-        renderingContext.fillStyle = "rgba(200,0,0,0.5)";
-        renderingContext.fillRect (10, 10, 150, 150);
+    renderingContext.fillStyle = "rgba(200,0,0,0.5)";
+    renderingContext.fillRect (10, 10, 150, 150);
  
-        renderingContext.fillStyle = "rgba(0, 200, 0,0.5)";
-        renderingContext.fillRect (50, 50, 150, 150);
+    renderingContext.fillStyle = "rgba(0, 200, 0,0.5)";
+    renderingContext.fillRect (50, 50, 150, 150);
 }());
