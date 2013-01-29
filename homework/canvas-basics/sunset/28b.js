5fe15800428816f0ef471a5a47ba4573211fe4a2
Chase Blokker <chaseblokker@yahoo.com>
2013-01-29 00:17:19 -0800

    cleaned up code. Created variables for easy manipulation of image.

--- homework/canvas-basics/sunset/28b.js
+++ homework/canvas-basics/sunset/28b.js
@@ -9,51 +9,57 @@
 
 
 (function () {
-    var canvas = document.getElementById('canvas');
-        var renderingContext = canvas.getContext("2d");
+    var canvas = document.getElementById('canvas'),
+        renderingContext = canvas.getContext("2d"),
 
-        // Define gradient variables
-        linearSkyGradient = renderingContext.createLinearGradient(0, 0, 0, 300);
-        radialSkyGradient = renderingContext.createRadialGradient(250, 200, 1, 250, 200, 200);
-        linearOceanGradient = renderingContext.createLinearGradient(0, 100, 0, 350);     
-        sunReflectionGradient = renderingContext.createLinearGradient(199, 0, 301, 0);
+        // Sunset variables
+        horizonPosition = 200,
+        sunRadius = 50;
 
-        // Colorstops for above gradients
-        linearSkyGradient.addColorStop(0, '#000077');
-        linearSkyGradient.addColorStop(1, "orange");
+    // Define gradient variables
+    linearSkyGradient = renderingContext.createLinearGradient(0, 0, 0, horizonPosition + 100);
+    radialSkyGradient = renderingContext.createRadialGradient((canvas.width / 2), horizonPosition, 1,
+                                                              (canvas.width / 2), 200, 200);
+    linearOceanGradient = renderingContext.createLinearGradient(0, horizonPosition - 100, 0, horizonPosition + 150);     
+    sunReflectionGradient = renderingContext.createLinearGradient((canvas.width / 2) - sunRadius - 2, 0,
+                                                                  (canvas.width / 2) + sunRadius + 2, 0);
 
-        radialSkyGradient.addColorStop(1, 'purple');
-        radialSkyGradient.addColorStop(0, "orange");
+    // Colorstops for above gradients
+    linearSkyGradient.addColorStop(0, '#000077');
+    linearSkyGradient.addColorStop(1, "orange");
 
-        linearOceanGradient.addColorStop(0, "transparent");
-        linearOceanGradient.addColorStop(1, "#000066");
+    radialSkyGradient.addColorStop(1, 'purple');
+    radialSkyGradient.addColorStop(0, "orange");
 
-        sunReflectionGradient.addColorStop(0, "transparent")
-        sunReflectionGradient.addColorStop(0.15, "orange")
-        sunReflectionGradient.addColorStop(0.85, "orange")
-        sunReflectionGradient.addColorStop(1, "transparent")
+    linearOceanGradient.addColorStop(0, "transparent");
+    linearOceanGradient.addColorStop(1, "#000066");
 
-        // The sky: a combination of both radial and linear gradient
-        renderingContext.globalAlpha = 0.5;
-        renderingContext.fillStyle = radialSkyGradient;
-        renderingContext.fillRect (0, 0, 512, 450);
-        renderingContext.fillStyle = linearSkyGradient;
-        renderingContext.fillRect (0, 0, 512, 200);
+    sunReflectionGradient.addColorStop(0, "transparent")
+    sunReflectionGradient.addColorStop(0.15, "orange")
+    sunReflectionGradient.addColorStop(0.85, "orange")
+    sunReflectionGradient.addColorStop(1, "transparent")
 
-        // Adjust globalAlpha transparency to almost 1 so that the sun reflection still
-        // shows up at the bottom of the canvas
-        renderingContext.globalAlpha = 0.9;
+    // The sky: a combination of both radial and linear gradient
+    renderingContext.globalAlpha = 0.5;
+    renderingContext.fillStyle = radialSkyGradient;
+    renderingContext.fillRect (0, 0, canvas.width, canvas.height);
+    renderingContext.fillStyle = linearSkyGradient;
+    renderingContext.fillRect (0, 0, canvas.width, horizonPosition);
 
-        // The orange sun
-        renderingContext.arc(250, 200, 50, 0, Math.PI, true);
-        renderingContext.fillStyle = 'orange';
-        renderingContext.fill();
+    // Adjust globalAlpha transparency to almost 1 so that the sun reflection still
+    // shows up at the bottom of the canvas
+    renderingContext.globalAlpha = 0.95;
 
-        // The sun reflection
-        renderingContext.fillStyle = sunReflectionGradient;
-        renderingContext.fillRect (0, 200, 512, 256);
-        
-        // The Ocean
-        renderingContext.fillStyle = linearOceanGradient;
-        renderingContext.fillRect(0,200,512,256);
+    // The orange sun
+    renderingContext.arc(canvas.width / 2, horizonPosition, sunRadius, 0, Math.PI, true);
+    renderingContext.fillStyle = 'orange';
+    renderingContext.fill();
+
+    // The sun reflection
+    renderingContext.fillStyle = sunReflectionGradient;
+    renderingContext.fillRect (0, horizonPosition, canvas.width, canvas.height);
+    
+    // The Ocean
+    renderingContext.fillStyle = linearOceanGradient;
+    renderingContext.fillRect(0, horizonPosition, canvas.width, canvas.height - horizonPosition);
 }());
