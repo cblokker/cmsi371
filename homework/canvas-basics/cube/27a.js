968d47f41bb2c8a4c5dc965f6be5eceba2d36072
Chase Blokker <chaseblokker@yahoo.com>
2013-01-28 23:44:37 -0800

    added comments.

--- homework/canvas-basics/cube/27a.js
+++ homework/canvas-basics/cube/27a.js
@@ -10,14 +10,18 @@
 (function () {
     var canvas = document.getElementById('canvas'),
         renderingContext = canvas.getContext("2d"),
+
+        // Cube variables
         topLeftBackCoordinate = [300, 250],
         topLeftFrontCoordinate = [350, 300],
         cubeLength = 150;
     
+    // Do the front and back of the cube (two overlapping sqaures)
     renderingContext.strokeStyle = 'green';
     renderingContext.strokeRect (topLeftBackCoordinate[0], topLeftBackCoordinate[1], cubeLength, cubeLength);
     renderingContext.strokeRect (topLeftFrontCoordinate[0], topLeftFrontCoordinate[1], cubeLength, cubeLength);
 
+    // Connect the corners of the two squares to form a wireframe cube
     for (var i = 0; i <= 1; i++) {
         renderingContext.translate(cubeLength * i, 0);
         renderingContext.beginPath();
